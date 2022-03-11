1cd78cdc9077ddce5b8394d0e2f40aea5c74df67
Diki setiawan <dikisetia101@gmail.com>
2022-03-11 22:07:55 +0700

    update file tgl 11 mar 22

---
+++ application/controllers/User.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class User extends CI_Controller
+{
+
+    public function index()
+    {
+        $this->load->view('user/index');
+    }
+}
